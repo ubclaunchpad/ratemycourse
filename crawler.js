663416be3bf64f8024b0e4a118718505ebf5ca2a
Luxi-Zhao <lucyzhao.zlx@gmail.com>
2017-10-07 15:53:09 -0700

    print course code, description to json file

--- crawler.js
+++ crawler.js
@@ -20,6 +20,7 @@ app.get('/scrape', function(req, res){
                 for (var depIndex in departments) {
                     if (departments.hasOwnProperty(depIndex)) {
                         var department = departments.eq(depIndex);
+                        var department_code = department.children().eq(0).children().eq(0).text();
                         var link = department.children().eq(0).children().eq(0).attr('href');
                         link = 'https://courses.students.ubc.ca' + link;
                         request(link, function(error, response, html) {
@@ -32,6 +33,7 @@ app.get('/scrape', function(req, res){
                                     for(var courseIndex in courses) {
                                         if(courses.hasOwnProperty(courseIndex)) {
                                             var course = courses.eq(courseIndex);
+                                            var course_code = course.children().eq(0).children().eq(0).text().trim();
                                             var course_link = course.children().eq(0).children().eq(0).attr('href');
                                             course_link = 'https://courses.students.ubc.ca' + course_link;                                          
                                             request(course_link, function(error, response, html) {
@@ -46,17 +48,32 @@ app.get('/scrape', function(req, res){
                                                                 var section = sections.eq(secIndex);
                                                                 var course_page = section.children().eq(1).children().eq(0);                                                    
                                                                 if(course_page.attr('href')) {
+                                                                    var sec_code = section.children().eq(1).children().eq(0).text().trim();
                                                                     var page_link = section.children().eq(1).children().eq(0).attr('href');
                                                                     page_link = 'https://courses.students.ubc.ca' + page_link;
                                                                     request(page_link, function(error, response, html) {                                                   
                                                                         if(!error) {
                                                                             var $ = cheerio.load(html);
-                                                                            var prof;
+                                                                            var json = { prof : "", section_code : "", title: "", description: ""};
+                                                                            
                                                                             $('tr:has(td:contains("Instructor:  "))').filter(function() {
                                                                                 var data = $(this);
-                                                                                prof = data.children().last().text();
-                                                                                console.log(prof);
+                                                                                var prof = data.children().last().text();
+                                                                                json.prof = prof;
+                                                                                json.section_code = sec_code;                                                                                                                   
                                                                             })
+                                                                            $('.content').filter(function() {
+                                                                                var data = $(this);
+                                                                                var title = data.children().eq(2).text();
+                                                                                json.title = title;
+                                                                                var description = data.children().eq(3).text();
+                                                                                json.description = description;
+                                                                            })
+                                                                            fs.appendFile('message.json', JSON.stringify(json, null, 4), function (err) {
+                                                                                    if (err) throw err;
+                                                                                    console.log('Saved!');
+                                                                            });
+                                                                            
                                                                         }
                                                                     })
                                                                 }

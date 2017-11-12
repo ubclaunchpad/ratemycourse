36961b389abf63b98be92ae7a662f3fd12d94222
Luxi Zhao <lucyzhao.zlx@gmail.com>
2017-11-12 00:21:39 -0800

    friends list done

---
+++ Coursify/app/src/main/java/com/example/coursify/FriendListAdapter.java
@@ -0,0 +1,60 @@
+package com.example.coursify;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by LucyZhao on 2017/11/11.
+ */
+
+public class FriendListAdapter extends RecyclerView.Adapter<FriendListAdapter.ViewHolder> {
+    private final List<User> userList;
+
+    public FriendListAdapter(List<User> userList) {
+        this.userList = userList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // create a new view
+        View itemLayoutView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.user_list_item, parent, false);
+
+        return new ViewHolder(itemLayoutView);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.txtFBUserName.setText(userList.get(position).getName());
+        holder.txtMajor.setText((userList.get(position).getMajor()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return userList.size();
+    }
+
+    // inner class to hold a reference to each item of RecyclerView
+    static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private TextView txtFBUserName;
+        private TextView txtMajor;
+
+
+        private ViewHolder(View itemLayoutView) {
+            super(itemLayoutView);
+            this.txtFBUserName = (TextView) itemLayoutView.findViewById(R.id.txtFBUserName);
+            this.txtMajor = (TextView) itemLayoutView.findViewById(R.id.txtMajor);
+        }
+
+        @Override
+        public void onClick(View view) {
+            //todo
+        }
+
+    }
+}
--- Coursify/app/src/main/java/com/example/coursify/ProfileSettings.java
+++ Coursify/app/src/main/java/com/example/coursify/ProfileSettings.java
@@ -1,10 +1,7 @@
 package com.example.coursify;
 import android.content.Intent;
-import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -20,17 +17,13 @@ import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
-import com.facebook.GraphRequest;
-import com.facebook.GraphResponse;
-import com.facebook.HttpMethod;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
-import static android.R.attr.name;
+import static com.example.coursify.Utils.processEmail;
 
 public class ProfileSettings extends AppCompatActivity {
     private static final String TAG = RegisterActivity.class.getSimpleName();
@@ -40,18 +33,7 @@ public class ProfileSettings extends AppCompatActivity {
     private AccessToken mAccessToken;
     private String mUserId;
 
-    public static class User {
-        public String major;
-        public String name;
-        public String gradDate;
-        public String facebookID;
-        public User(String name, String major, String gradDate, String facebookID){
-            this.major = major;
-            this.name = name;
-            this.gradDate = gradDate;
-            this.facebookID = facebookID;
-        }
-    }
+
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         mCallBackManager.onActivityResult(requestCode, resultCode, data);
@@ -98,7 +80,7 @@ public class ProfileSettings extends AppCompatActivity {
         Log.v(TAG, "and my facebook user id in set Facebook UserId is:" + mUserId);
         DatabaseReference firebasereference, userReference;
         firebasereference = FirebaseDatabase.getInstance().getReference();
-        String processedEmail = processString(email);
+        String processedEmail = processEmail(email);
         userReference = firebasereference.child("FacebookUsers").child(mUserId);
         userReference.setValue(processedEmail);
     }
@@ -119,7 +101,7 @@ public class ProfileSettings extends AppCompatActivity {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for(DataSnapshot ds : dataSnapshot.getChildren()){
-                    if(ds.getKey().toString().equals(processString(email))) {
+                    if(ds.getKey().toString().equals(processEmail(email))) {
                         Log.v(TAG, "found user");
                         HashMap<String, String> currUser = (HashMap)ds.getValue();
                         Log.v(TAG, "i WAS HERE");
@@ -152,7 +134,7 @@ public class ProfileSettings extends AppCompatActivity {
     protected void updateProfile(String email, String name, String major, String gradDate, String facebookID){
         DatabaseReference firebasereference, userReference;
         firebasereference = FirebaseDatabase.getInstance().getReference();
-        String processedEmail = processString(email);
+        String processedEmail = processEmail(email);
         userReference = firebasereference.child("Users").child(processedEmail);
         if(name.length() == 0 || major.length() == 0 || gradDate.length() == 0){
             Log.v(TAG, "You need to fill out all of name, major and gradDate fields");
@@ -161,14 +143,5 @@ public class ProfileSettings extends AppCompatActivity {
         }
     }
 
-    protected String processString(String email){
-        int i = email.indexOf('@');
-        email = email.substring(0, i) + ";at;" + email.substring(i+1);
-        for(int k = 0; k < email.length(); k++){
-            if(email.charAt(k) == '.'){
-                email = email.substring(0, k) + ";dot;" + email.substring(k+1);
-            }
-        }
-        return email;
-    }
+
 }

b8340c1c49948741e61affd859a5cba27dacf1fc
Luxi Zhao <lucyzhao.zlx@gmail.com>
2017-11-12 14:26:20 -0800

    checks if access token is null and gives a dialog

--- Coursify/app/src/main/java/com/example/coursify/ProfileActivity.java
+++ Coursify/app/src/main/java/com/example/coursify/ProfileActivity.java
@@ -1,22 +1,35 @@
 package com.example.coursify;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.transition.Slide;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.HttpMethod;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -29,6 +42,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -36,14 +50,61 @@ public class ProfileActivity extends AppCompatActivity {
     private static final String TAG = ProfileActivity.class.getSimpleName();
     private static final String FIREBASE_FBUSERS_ENDPOINT = "FacebookUsers";
     private static final String FIREBASE_USERS_ENDPOINT = "Users";
-    private static final String FIREBASE_USERS_FRIENDS_ENDPOINT = "FacebookFriends";
+    private static final String FIREBASE_USERS_FRIENDS_ENDPOINT = "facebookFriends";
 
+    CallbackManager mCallBackManager;
+    DialogFragment fbFragment;
 
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallBackManager.onActivityResult(requestCode, resultCode, data);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        FacebookSdk.sdkInitialize(getApplicationContext());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        LoginButton mLoginButton;
+        mLoginButton = (LoginButton) findViewById(R.id.btnFBLogin);
+        mLoginButton.setReadPermissions("user_friends", "email");
+        mCallBackManager = CallbackManager.Factory.create();
+
+        mLoginButton.registerCallback(mCallBackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.v(TAG, "Successfully connected to FaceBook.");
+            }
+
+            @Override
+            public void onCancel() {
+                Log.v(TAG, "Cancelled login to FaceBook.");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.v(TAG, "Error logging in to FaceBook");
+                Log.v(TAG, error.getMessage());
+            }
+        });
+        LoginManager.getInstance().registerCallback(mCallBackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.v(TAG, "Successfully connected to FaceBook.");
+            }
+
+            @Override
+            public void onCancel() {
+                Log.v(TAG, "Cancelled login to FaceBook.");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.v(TAG, "Error logging in to FaceBook");
+                Log.v(TAG, error.getMessage());
+            }
+        });
 
         FBFriendsFragment friendsFragment = new FBFriendsFragment();
         FragmentTransaction transaction = getFragmentManager().beginTransaction();
@@ -54,11 +115,96 @@ public class ProfileActivity extends AppCompatActivity {
         transaction.commit();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v(TAG, "ON RESUME");
+        FBFriendsFragment friendsFragment = new FBFriendsFragment();
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+        // Replace whatever is in the fragment_container view with this fragment,
+        // and add the transaction to the back stack if needed
+        transaction.add(R.id.activity_profile, friendsFragment);
+        transaction.commit();
+    }
+
+    void showDialog() {
+        fbFragment = MyAlertDialogFragment.newInstance(
+                R.string.alert_dialog_title);
+        fbFragment.show(getFragmentManager(), "dialog");
+    }
+
+    public void doPositiveClick() {
+        // Do stuff here.
+        Log.i("FragmentAlertDialog", "Positive click!");
+        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("user_friends", "email"));
+    }
+
+    public void doNegativeClick() {
+        // Do stuff here.
+        Log.i("FragmentAlertDialog", "Negative click!");
+        fbFragment.dismiss();
+    }
+
+    public static class MyAlertDialogFragment extends DialogFragment {
+
+        public static MyAlertDialogFragment newInstance(int title) {
+            MyAlertDialogFragment frag = new MyAlertDialogFragment();
+            Bundle args = new Bundle();
+            args.putInt("title", title);
+            frag.setArguments(args);
+            return frag;
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            int title = getArguments().getInt("title");
+
+            return new AlertDialog.Builder(getActivity())
+                    .setTitle(title)
+                    .setPositiveButton("OK",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int whichButton) {
+                                    ((ProfileActivity)getActivity()).doPositiveClick();
+                                }
+                            }
+                    )
+                    .setNegativeButton("CANCEL",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int whichButton) {
+                                    ((ProfileActivity)getActivity()).doNegativeClick();
+                                }
+                            }
+                    )
+                    .create();
+        }
+    }
+
     public static class FBFriendsFragment extends Fragment {
         List<User> userList = new ArrayList<>();
         FriendListAdapter friendListAdapter;
         DatabaseReference ref;
         private FirebaseAuth mAuth;
+        private AccessToken accessToken;
+        private AccessTokenTracker accessTokenTracker;
+
+        @Override
+        public void onCreate (Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            accessTokenTracker = new AccessTokenTracker() {
+                @Override
+                protected void onCurrentAccessTokenChanged (
+                        AccessToken oldAccessToken,
+                        AccessToken currentAccessToken) {
+                    // Set the access token using
+                    // currentAccessToken when it's loaded or set.
+                    Log.v(TAG, "access token changed");
+                    accessToken = currentAccessToken;
+                }
+            };
+            accessToken = AccessToken.getCurrentAccessToken();
+        }
+
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -83,7 +229,7 @@ public class ProfileActivity extends AppCompatActivity {
                         public void onDataChange(DataSnapshot dataSnapshot) {
                             String fbID = dataSnapshot.getValue(String.class);
                             Log.v(TAG, "fb id is: " + fbID);
-                            showFriends(fbID);
+                            getFBFriends(fbID);
                         }
 
                         @Override
@@ -92,10 +238,13 @@ public class ProfileActivity extends AppCompatActivity {
                         }
                     });
 
-
-
-
             return fragmentView;
+        }
+
+        @Override
+        public void onDestroy() {
+            super.onDestroy();
+            accessTokenTracker.stopTracking();
         }
 
         private void getFriendInfo(String fbID) {
@@ -142,42 +291,52 @@ public class ProfileActivity extends AppCompatActivity {
                     });
         }
 
-        private void showFriends(String mFBUserId) {
-            /* make the API call */
-            new GraphRequest(
-                    AccessToken.getCurrentAccessToken(),
-                    "/" + mFBUserId + "/friends",
-                    null,
-                    HttpMethod.GET,
-                    new GraphRequest.Callback() {
-                        public void onCompleted(GraphResponse response) {
-                            JSONObject object = response.getJSONObject();
-                            try {
-                                JSONArray arrayOfUsersInFriendList = object.getJSONArray("data");
-                                Log.v(TAG, "User friend list length: " + arrayOfUsersInFriendList.length());
-                                List<String> fbFriends = new ArrayList<>();
+        /**
+         * Fetch a list of Facebook friends using Facebook SDK
+         * Update the user/facebookFriends field in Firebase with
+         * the new list
+         * @param mFBUserId
+         */
+        private void getFBFriends(String mFBUserId) {
+            if(accessToken == null) {
+       //         Toast.makeText(getActivity(), "please login to facebook", Toast.LENGTH_LONG).show();
+                ((ProfileActivity)getActivity()).showDialog();
+            }
+            else {
+                new GraphRequest(
+                        accessToken,
+                        "/" + mFBUserId + "/friends",
+                        null,
+                        HttpMethod.GET,
+                        new GraphRequest.Callback() {
+                            public void onCompleted(GraphResponse response) {
+                                JSONObject object = response.getJSONObject();
+                                try {
+                                    JSONArray arrayOfUsersInFriendList = object.getJSONArray("data");
+                                    Log.v(TAG, "User friend list length: " + arrayOfUsersInFriendList.length());
+                                    List<String> fbFriends = new ArrayList<>();
 
-                                for(int i = 0; i < arrayOfUsersInFriendList.length(); i++) {
-                                    JSONObject user = arrayOfUsersInFriendList.getJSONObject(i);
-                                    String userId = user.getString("id");
-                                    Log.v(TAG, "user id: " + userId);
-                                    Log.v(TAG, "user name is " + user.getString("name"));
-                                    getFriendInfo(userId);
-                                    fbFriends.add(userId);
+                                    for(int i = 0; i < arrayOfUsersInFriendList.length(); i++) {
+                                        JSONObject user = arrayOfUsersInFriendList.getJSONObject(i);
+                                        String userId = user.getString("id");
+                                        Log.v(TAG, "user id: " + userId);
+                                        Log.v(TAG, "user name is " + user.getString("name"));
+                                        getFriendInfo(userId);
+                                        fbFriends.add(userId);
+                                    }
+                                    ref.child(FIREBASE_USERS_ENDPOINT)
+                                            .child(Utils.processEmail(mAuth.getCurrentUser().getEmail()))
+                                            .child(FIREBASE_USERS_FRIENDS_ENDPOINT)
+                                            .setValue(fbFriends);
+
+                                } catch (JSONException e) {
+                                    Log.v(TAG, e.toString());
+                                    e.printStackTrace();
                                 }
-                                ref.child(FIREBASE_USERS_ENDPOINT)
-                                        .child(Utils.processEmail(mAuth.getCurrentUser().getEmail()))
-                                        .child(FIREBASE_USERS_FRIENDS_ENDPOINT)
-                                        .setValue(fbFriends);
-
-                            } catch (JSONException e) {
-                                Log.v(TAG, e.toString());
-                                e.printStackTrace();
                             }
                         }
-                    }
-            ).executeAsync();
-
+                ).executeAsync();
+            }
         }
     }
 }

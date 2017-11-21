2318842471136ec2e3f72107a355e4f307e3a5f0
Annie Zhou <annie_0812@live.ca>
2017-11-20 18:49:54 -0800

    restructured course - if easiness and usefulness are both -1 then no rating has been done

--- Coursify/app/src/main/java/com/example/coursify/Course.java
+++ Coursify/app/src/main/java/com/example/coursify/Course.java
@@ -10,15 +10,20 @@ public class Course {
 
     String courseTitle;
     String courseCode;
+    String description;
     List<String> takenUsers;
     List<String> goingUsers;
     List<String> interestedUsers;
     List<Comment> comments;
     List<Rating> ratings;
+    double easiness;
+    double usefulness;
 
-    public Course (String code, String title) {
+    public Course (String code, String title, String description) {
         courseTitle = title;
         courseCode = code;
+        easiness = -1;
+        usefulness = -1;
     }
 
 }

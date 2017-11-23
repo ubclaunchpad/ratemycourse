34dcb41bbf85507b1aa207d3604d830ecd1d1e0f
Annie Zhou <annie_0812@live.ca>
2017-11-22 17:41:09 -0800

    added pinned field in Notes

--- Coursify/app/src/main/java/com/example/coursify/Note.java
+++ Coursify/app/src/main/java/com/example/coursify/Note.java
@@ -11,10 +11,18 @@ class Note {
     String color;
     String content;
     String time;
+    boolean pinned;
 
     public Note(String color, String content){
         this.color = color;
         this.content = content;
         this.time = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+    }
+
+    public Note(String color, String content, boolean pinned){
+        this.color = color;
+        this.content = content;
+        this.time = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+        this.pinned = pinned;
     }
 }

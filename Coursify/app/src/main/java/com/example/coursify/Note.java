76ac9338ac6377515f1e6e414208980fb6e5c199
Annie Zhou <annie_0812@live.ca>
2017-11-22 17:43:20 -0800

    added editing note method in Notes

--- Coursify/app/src/main/java/com/example/coursify/Note.java
+++ Coursify/app/src/main/java/com/example/coursify/Note.java
@@ -25,4 +25,11 @@ class Note {
         this.time = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
         this.pinned = pinned;
     }
+
+    public void edit(String new_color, String new_content, boolean new_pinned){
+        this.color = new_color;
+        this.content = new_content;
+        this.pinned = new_pinned;
+        this.time = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+    }
 }
